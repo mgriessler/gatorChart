9a5ba2506ea083d53682a7bcfd2563cc497116bb
Valentina Rendon <valentina_021992@hotmail.com>
2015-03-30 20:35:19 -0400

    Revert "shape.h"
    This reverts commit 4b727489f18719bbc33dbe4ebbe3fe0e6bfc466b.

--- shape.h
+++ shape.h
@@ -1,45 +1,12 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
-#include <QGraphicsPolygonItem>
-#include <QMenu>
-#include <QGraphicsItem>
-#include <QGraphicsScene>
-#include <QPolygonF>
-#include <QPixmap>
-#include <QGraphicsSceneContextMenuEvent>
-#include <QVariant>;
-#include <QList>
 
-class shape : public QGraphicsPolygonItem
+class shape
 {
 public:
-    enum{Type = UserType + 15};
-    enum DiagramType {Step, Conditional, StartEnd, Io};
-
-    shape(DiagramType diagramType, QMenu *contextMenu, QGraphicsItem *parent = 0, QGraphicsScene *scene = 0);
-
-    void removeArrow(Arrow *arrow);
-    void removeArrow();
-    DiagramType diagramType() const
-        {return myDiagramType;}
-    QPolygonF polygon() const
-        {return myPolygon;}
-    void addArrow(Arrow *arrow);
-    QPixmap image() const;
-    int type() const
-        {return Type;}
-
-protected:
-    void contextMenuEvent(QGraphicsSceneContextMenuEvent *event);
-    QVariant itemChange(GraphicsItemChange change, const QVariant &value);
-
-private:
-    DiagramType myDiagramType;
-    QPolygonF myPolygon;
-    QMenu *myContextMenu;
-    QList<Arrrow*> arrows;
-
+    shape();
+    ~shape();
 };
 
 #endif // SHAPE_H
