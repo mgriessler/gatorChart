4cc5715c6fd67653cfa9543c8cae99786fc30f18
Valentina Rendon <valentina_021992@hotmail.com>
2015-03-30 20:36:48 -0400

    Taking arrow away

--- arrow.h
+++ arrow.h
@@ -1,12 +1,87 @@
 #ifndef ARROW_H
 #define ARROW_H
 
+#include <QGraphicsLineItem>
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QRectF>
+#include <QPainterPath>
+#include <QColor>
+#include <QPainter>
+#include <QStyleOptionGraphicsItem>
+#include <QWidget>
+#include <QPolygonF>
 
-class arrow
+
+class Arrow : public QGraphicsLineItem
+
 {
-public:
-    arrow();
-    ~arrow();
-};
 
-#endif // ARROW_H
+
+public:
+
+
+    enum  { Type = UserType + 4 };
+
+    Arrow(shape *startItem, shape *endItem,
+
+      QGraphicsItem *parent = 0, QGraphicsScene *scene = 0);
+
+    int type() const
+
+
+         { return Type; }
+
+
+    QRectF boundingRect() const;
+
+    QPainterPath Shape() const;
+
+
+    void setColor(const QColor &color)
+
+
+       { myColor = color; }
+
+
+    shape *startItem() const
+
+
+         { return myStartItem; }
+
+
+    shape *endItem() const
+
+
+        { return myEndItem; }
+
+
+    void updatePosition();
+
+
+
+protected:
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+
+               QWidget *widget = 0);
+
+
+
+
+private:
+
+
+    shape *myStartItem;
+
+
+    shape *myEndItem;
+
+
+    QColor myColor;
+
+
+    QPolygonF arrowHead;
+
+
+};
--- shape.cpp
+++ shape.cpp
@@ -1,12 +1,104 @@
 #include "shape.h"
+#include <QMenu>
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QGraphicsPolygonItem>
+#include <QPainterPath>
+#include <QPointF>
+#include <QPixmap>
+#include <Qt>
+#include <QPainter>
+#include <QPen>
+#include <QGraphicsSceneContextMenuEvent>
+#include <QVariant>
 
-shape::shape()
-{
 
-}
 
-shape::~shape()
-{
 
-}
+//Constructor
 
+    shape::shape(DiagramType diagramType, QMenu *contextMenu, QGraphicsIten *parent, QgraphicsScene *scene) :QGraphicsPolygonItem(parent, scene)
+    {
+        myDiagramType = diagramType;
+        myContextMenu = contextMenu;
+
+        QPainterPath path;
+        switch (myDiagramType)
+            switch(myDiagramType){
+                case StartEnd:
+                    path.moveTo(200, 50);
+                    path.arcTo(150, 0, 50, 50, 0, 90);
+                    path.arcTo(50, 0, 50, 50, 90, 90);
+                    path.arcTo(50, 50, 50, 50, 180, 90);
+                    path.arcTo(150, 50, 50, 50, 270, 90);
+                    path.lineTo(200, 25);
+                    myPolygon = path.toFillPolygon();
+                    break;
+                case Conditional:
+                    myPolygon << QPointF(-100, 0) << QPointF(0, 100) << QPointF(100, 0) << QPointF(0, -100) << QPointF(-100, 0);
+                    break;
+                case Step:
+                    myPolygon << QPointF(-100, -100) << QPointF(100, -100) << QPointF(100, 100) << QPointF(-100, 100) << QPointF(-100, -100);
+                    break;
+                default:
+                    myPolygon << QPointF(-120, -80) << QPointF(-70, 80) << QPointF(120, 80) << QPointF(70, -80) << QPointF(-120, -80);
+                    break;
+
+            }
+    setPolygon(myPolygon);
+    setFlag(QGraphicItem::ItemIsMovable, true);
+    setFlag(QGraphicItem::ItemIsSelectable, true);
+    setFlag(QGraphicItem::ItemSendsGeometryChanges, true);
+    }
+
+    //removeArrow() function
+
+    void shape::removeArrow(Arrow *arrow)
+    {
+        int index = arrows.indexOf(arrow);
+
+        if(index != -1)
+            arrows.removeAt(index);
+    }
+
+    //addArrow() function
+
+    void shape::addArrow(Arrow *arrow)
+    {
+        arrows.append(arrow);
+    }
+
+    //image() function
+
+    QPixmap shape::image() const
+    {
+        QPixmap pixmap(250, 250);
+        pixmap.fill(Qt::transparent);
+        QPainter painter(&pixmap);
+        painter.setPen(QPen(Qt::black, 8));
+        painter.translate(125, 125);
+        painter.drawPolyline(myPolygon);
+
+        return pixmap;
+    }
+
+    //contextMenuEvent() function
+
+    void shape::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+    {
+        scene()->clearSelection();
+        setSelected(true);
+        myContextMenu->exec(event->screenPos());
+    }
+
+    //implementation of itemChange()
+
+    QVariant shape::itemChange(GraphicsItemChange change, const QVariant &value)
+    {
+        if(change == QGraphicsItem::ItemPositionChange){
+            foreach(Arrow *arrow, arrows){
+                arrow->updatePosition();
+            }
+        }
+        return value;
+    }
